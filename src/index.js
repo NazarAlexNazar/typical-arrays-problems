b5b5a6872b592f214b0a8b4d7014205c350929cf
NazarAlexNazar <vitalik.7625@mail.ru>
2020-09-30 09:20:43 +0300

    add min, mam function

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+exports.min = function min (myArr) {
+  if ((myArr === null) || (myArr === undefined) || (!Array.isArray(myArr)) || (myArr.length === 0)) return 0;
+
+  myArr.sort(function(a, b) {
+    return a - b;
+  });
+
+  return myArr[0];
+}
+
+exports.max = function max (myArr) {
+  if ((myArr === null) || (myArr === undefined) || (!Array.isArray(myArr)) || (myArr.length === 0)) return 0;
+
+  myArr.sort(function(a, b) {
+    return a - b;
+  });
+
+  return myArr[myArr.length-1];
+}
+
+exports.avg = function avg (myArr) {
+  return 0;
+}
